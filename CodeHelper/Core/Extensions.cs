65b39271713aa3cd9b66df928436426b69fc8f3b
Никита Целков <nikitacelkov@gmail.com>
2023-11-21 12:14:45 +0700

    Updated Extentions

--- CodeHelper/Core/Extensions.cs
+++ CodeHelper/Core/Extensions.cs
@@ -18,5 +18,61 @@ namespace CodeHelper.Core
         {
             return JsonConvert.DeserializeObject<T>(value, _settings);
         }
+
+        public static string CalculateNumber(int num)
+        {
+            if (num >= 1000000)
+            {
+                return (num / 1000000d).ToString("0.#") + "m";
+            }
+            else if (num >= 1000)
+            {
+                return (num / 1000d).ToString("0.#") + "k";
+            }
+            else
+            {
+                return num.ToString();
+            }
+        }
+
+        public static string CalculateTimeElapsed(DateTime inputDate, DateTime currentDate)
+        {
+            TimeSpan timeSpan = currentDate - inputDate;
+            int years = currentDate.Year - inputDate.Year;
+            int months = currentDate.Month - inputDate.Month;
+            int days = currentDate.Day - inputDate.Day;
+
+            if (months < 0 || (months == 0 && days < 0))
+            {
+                years--;
+                months = (currentDate.Month + 12) - inputDate.Month;
+            }
+
+            if (days < 0)
+            {
+                int previousMonth = currentDate.Month - 1;
+                if (previousMonth == 0) previousMonth = 12;
+                int daysInPreviousMonth = DateTime.DaysInMonth(currentDate.Year, previousMonth);
+                days = daysInPreviousMonth + days;
+                months--;
+            }
+
+            if (years > 0)
+            {
+                return $"{years} {(years == 1 ? "year" : "years")} ago";
+            }
+            else if (months > 0)
+            {
+                return $"{months} {(months == 1 ? "month" : "months")} ago";
+            }
+            else if (days > 0)
+            {
+                return $"{days} {(days == 1 ? "day" : "days")} ago";
+            }
+            else
+            {
+                return "Today";
+            }
+        }
     }
 }
--- CodeHelper/ViewModels/QuestionViewModel.cs
+++ CodeHelper/ViewModels/QuestionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CodeHelper.Models.Domain;
+using CodeHelper.Core;
 
 namespace CodeHelper.ViewModels
 {
@@ -13,50 +14,10 @@ namespace CodeHelper.ViewModels
             {
                 if (Question?.PublisedDate != null)
                 {
-                    return CalculateTimeElapsed(Question.PublisedDate, DateTime.UtcNow.Date);
+                    return Extensions.CalculateTimeElapsed(Question.PublisedDate, DateTime.UtcNow.Date);
                 }
 
                 return string.Empty;
-            }
-        }
-
-        private string CalculateTimeElapsed(DateTime inputDate, DateTime currentDate)
-        {
-            TimeSpan timeSpan = currentDate - inputDate;
-            int years = currentDate.Year - inputDate.Year;
-            int months = currentDate.Month - inputDate.Month;
-            int days = currentDate.Day - inputDate.Day;
-
-            if (months < 0 || (months == 0 && days < 0))
-            {
-                years--;
-                months = (currentDate.Month + 12) - inputDate.Month;
-            }
-
-            if (days < 0)
-            {
-                int previousMonth = currentDate.Month - 1;
-                if (previousMonth == 0) previousMonth = 12;
-                int daysInPreviousMonth = DateTime.DaysInMonth(currentDate.Year, previousMonth);
-                days = daysInPreviousMonth + days;
-                months--;
-            }
-
-            if (years > 0)
-            {
-                return $"{years} {(years == 1 ? "year" : "years")} ago";
-            }
-            else if (months > 0)
-            {
-                return $"{months} {(months == 1 ? "month" : "months")} ago";
-            }
-            else if (days > 0)
-            {
-                return $"{days} {(days == 1 ? "day" : "days")} ago";
-            }
-            else
-            {
-                return "Today";
             }
         }
     }
